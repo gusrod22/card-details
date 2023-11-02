2ef7e72daf41c295e26a4f4d907bafb317563c61
rodjus19@gmail.com <gusrod22>
2023-11-01 21:58:05 -0400

    updated script

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const submitButton = document.querySelector("#submit-button");
 const form = document.querySelector("form");
+
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
+  console.log("button clicked");
+
   const inputItems = form.querySelectorAll(".input-item");
+
   for (const inputItem of inputItems) {
     const input = inputItem.querySelector("input");
     const error = inputItem.querySelector(".error");
-    const cardNumber = document.getElementById("card-number");
-    const mmInput = document.getElementById("mm");
-    const yyInput = document.getElementById("yy");
-    const cvcInput = document.getElementById("cc");
     if (!input.value) {
       if (error) {
         error.style.display = "block";
       }
       input.classList.add("input-error");
+    } else {
+      if (error) {
+        error.style.display = "none";
+      }
+      input.classList.remove("input-error");
     }
   }
 });
