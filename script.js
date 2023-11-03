66cfbfeb6a5a52d0217d699e005a45c18ab9c6c8
rodjus19@gmail.com <gusrod22>
2023-11-02 22:15:58 -0400

    added verification to check for correct format of card-number

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const submitButton = document.querySelector("#submit-button");
 const form = document.querySelector("form");
-
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   console.log("button clicked");
-
   const inputItems = form.querySelectorAll(".input-item");
-
   for (const inputItem of inputItems) {
     const input = inputItem.querySelector("input");
     const error = inputItem.querySelector(".error");
-    if (!input.value) {
-      if (error) {
-        error.style.display = "block";
+
+    if (input.id === "card-number") {
+      const cardNumberValue = input.value;
+      const pattern = /^\d{4} \d{4} \d{4} \d{4}$/;
+
+      if (!pattern.test(cardNumberValue)) {
+        if (error) {
+          error.style.display = "block";
+        }
+        input.classList.add("input-error");
+      } else {
+        if (error) {
+          error.style.display = "none";
+        }
+        input.classList.remove("input-error");
       }
-      input.classList.add("input-error");
     } else {
-      if (error) {
-        error.style.display = "none";
+      if (!input.value) {
+        if (error) {
+          error.style.display = "block";
+        }
+        input.classList.add("input-error");
+      } else {
+        if (error) {
+          error.style.display = "none";
+        }
+        input.classList.remove("input-error");
       }
-      input.classList.remove("input-error");
     }
   }
 });
